cbe54152e74cabd442809258121e9fc12807394d
lancecopper <she47637370@gmail.com>
2018-09-15 12:51:58 +0800

    extendible_hash_test revised

--- sqlite-fall2017/test/hash/extendible_hash_test.cpp
+++ sqlite-fall2017/test/hash/extendible_hash_test.cpp
@@ -77,7 +77,7 @@ TEST(ExtendibleHashTest, ConcurrentRemoveTest) {
   for (int run = 0; run < num_runs; run++) {
     std::shared_ptr<ExtendibleHash<int, int>> test{new ExtendibleHash<int, int>(2)};
     std::vector<std::thread> threads;
-    std::vector<int> values{0, 2, 4, 8, 16, 32, 64};
+    std::vector<int> values{0, 10, 16, 32, 64};
     for (int value : values) {
       test->Insert(value, value);
     }
@@ -85,7 +85,7 @@ TEST(ExtendibleHashTest, ConcurrentRemoveTest) {
     for (int tid = 0; tid < num_threads; tid++) {
       threads.push_back(std::thread([tid, &test, &values]() {
         test->Remove(values[tid]);
-        //test->Insert(tid + 4, tid + 4);
+        test->Insert(tid + 4, tid + 4);
       }));
     }
     for (int i = 0; i < num_threads; i++) {
@@ -94,11 +94,10 @@ TEST(ExtendibleHashTest, ConcurrentRemoveTest) {
     EXPECT_EQ(test->GetGlobalDepth(), 6);
     int val;
     EXPECT_EQ(0, test->Find(0, val));
-    EXPECT_EQ(0, test->Find(8, val));
+    EXPECT_EQ(1, test->Find(8, val));
     EXPECT_EQ(0, test->Find(16, val));
     EXPECT_EQ(0, test->Find(3, val));
-    EXPECT_EQ(0, test->Find(4, val));
+    EXPECT_EQ(1, test->Find(4, val));
   }
 }
-
 } // namespace cmudb
